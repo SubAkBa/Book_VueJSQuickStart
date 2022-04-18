0b6652841da68c43fda19418f4bae50be416f661
SubAkBa <circle5926@gmail.com>
2022-04-18 11:53:23 +0900

    7-10. 모듈

---
+++ chapter7/build/ex10.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var _ex10_util = require('./ex10_util1');
+
+console.log((0, _ex10_util.add)(4, 5));
+console.log(_ex10_util.var1);
---
+++ chapter7/src/ex10.js
@@ -0,0 +1,8 @@
+import { add, var1 as v } from './ex10_util1';
+import calc from './ex10_util2';
+
+console.log(add(4, 5));
+console.log(v);
+
+console.log(calc.add(4, 5));
+console.log(calc.multiply(4, 5));
