3d3e6598abe0cbccee318fb01afec58352420026
SubAkBa <circle5926@gmail.com>
2022-04-19 18:48:04 +0900

    9-1. 단일 파일 컴포넌트

---
+++ chapter9/todolistapp/src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="todolistapp">
+    <div id="header" class="header">
+      <h2>Todo List APP</h2>
+      <input-todo></input-todo>
+    </div>
+    <list-item></list-item>
+  </div>
+</template>
+
+<script>
+import InputTodo from "@/components/inputTodo";
+import ListItem from "@/components/ListItem";
+
+export default {
+  name: "TodoList",
+  components: {
+    InputTodo,
+    ListItem
+  }
+}
+</script>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+.header {
+  background-color: purple;
+  padding: 30px;
+  color: yellow;
+  text-align: center;
+}
+
+.header:after {
+  content: '';
+  display: table;
+  clear: both;
+}
+</style>
---
+++ chapter9/todolistapp/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import todoList from "@/components/TodoList";
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(todoList),
+}).$mount('#app')
