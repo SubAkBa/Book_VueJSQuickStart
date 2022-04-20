4ac7a64460fd65c48816b7ab2662623ac36b8ecc
SubAkBa <circle5926@gmail.com>
2022-04-20 11:25:10 +0900

    9-5. 재귀 컴포넌트

---
+++ chapter9/dynamictest/src/components/Tree.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li v-for="s in subs" v-bind:class="s.type" :key="s.name">
+      {{ s.name }}
+      <tree-item :subs="s.subs"></tree-item>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "TreeItem",
+  props: [ 'subs' ]
+}
+</script>
+
+<style scoped>
+
+</style>
