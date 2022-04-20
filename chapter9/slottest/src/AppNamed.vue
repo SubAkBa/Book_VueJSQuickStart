83b219394be29603b859bb33b9564c42061f7368
SubAkBa <circle5926@gmail.com>
2022-04-20 10:51:46 +0900

    9-3. 슬롯

---
+++ chapter9/slottest/src/AppNamed.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <layout>
+      <h1 slot="header">헤더 영역</h1>
+
+      <div slot="sidebar">
+        <ul class="menu">
+          <li v-for="sidebar in sidebars" :key="sidebar.menu">
+            <a v-bind:href="sidebar.link">{{ sidebar.menu }}</a>
+          </li>
+        </ul>
+      </div>
+    </layout>
+  </div>
+</template>
+
+<script>
+import Layout from './components/NamedSlot'
+
+export default {
+  name: "AppNamed",
+  components: {
+    Layout
+  },
+  data() {
+    return {
+      sidebars: [
+        {menu: 'Home', link: '#'},
+        {menu: 'About', link: '#'},
+        {menu: 'Contact', link: '#'},
+        {menu: 'Vue.js', link: '#'},
+      ]
+    }
+  },
+}
+</script>
+
+<style scoped>
+ul.menu {
+  position: relative;
+  padding: 5px;
+  list-style: none;
+  font-style: italic;
+}
+
+ul.menu a {
+  text-decoration: none;
+}
+</style>
