43e145babfdb3f0b9f75d1794e9ad0aeb29205ab
SubAkBa <circle5926@gmail.com>
2022-04-28 17:57:02 +0900

    11-5-2. 액션을 이용한 비동기 처리

---
+++ chapter11/contactsapp_search/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <search-item placeholder="두글자 이상 입력후 엔터!"></search-item>
+    <contact-list></contact-list>
+  </div>
+</template>
+
+<script>
+import ContactList from './components/ContactList';
+import SearchItem from './components/SearchItem';
+
+export default {
+  name: 'App',
+  components: {
+    ContactList,
+    SearchItem
+  }
+}
+</script>
+
+<style>
+</style>
