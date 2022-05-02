bd79abbae3200fa016c25b0b62e07bf586e9e4f3
SubAkBa <circle5926@gmail.com>
2022-05-02 09:39:11 +0900

    11-7-1. 상수 정의

---
+++ chapter11/contactsapp/src/components/AddContact.vue
@@ -0,0 +1,18 @@
+<template>
+  <contact-form mode="add" />
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm";
+
+export default {
+  name: "AddContact",
+  components: {
+    ContactForm
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ chapter11/contactsapp/src/components/UpdateContact.vue
@@ -0,0 +1,21 @@
+<template>
+  <contact-form mode="update" :contact="contact" />
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm";
+
+export default {
+  name: "UpdateContact",
+  components: {
+    ContactForm
+  },
+  props: [
+      'contact'
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
