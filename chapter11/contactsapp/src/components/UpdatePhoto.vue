72b7536322a329491d2e84c5be0b91b5b98853fa
SubAkBa <circle5926@gmail.com>
2022-05-03 10:49:42 +0900

    11-7-6. UpdatePhoto.vue 변경

--- chapter11/contactsapp/src/components/UpdatePhoto.vue
+++ chapter11/contactsapp/src/components/UpdatePhoto.vue
@@ -27,23 +27,20 @@
 </template>
 
 <script>
-import eventBus from '../EventBus'
+import Constant from '../Constant';
+import { mapState } from 'vuex';
 
 export default {
   name: "UpdatePhoto",
   props: ['contact'],
-  computed: {
-    contactOne() {
-      return this.contact;
-    },
-  },
+  computed: mapState(['contact']),
   methods: {
     cancelEvent() {
-      eventBus.$emit('cancel');
+      this.$store.dispatch(Constant.CANCEL_FORM);
     },
     photoSubmit() {
       var file = this.$refs.photofile.files[0];
-      eventBus.$emit('updatePhoto', this.contact.no, file);
+      this.$store.dispatch(Constant.UPDATE_PHOTO, { no: this.contact.no, file: file });
     },
   }
 }
